04f70c330618e35bb2b418521eec25880241a314
Alex Howard <alex-m-howard@pm.me>
2023-03-26 20:50:41 -0400

    Changed grid for component bleed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-import About from '@/components/About'
-import Projects from '@/components/Projects'
-import Contact from '@/components/Contact'
-import Resume from '@/components/Resume'
-import Profile from '@/components/Profile'
-import NavBar from '@/components/NavBar'
-import ToolBar from '@mui/material/Toolbar'
+import Head from 'next/head';
+import { Inter } from '@next/font/google';
+import styles from '@/styles/Home.module.css';
+import About from '@/components/About';
+import Projects from '@/components/Projects';
+import Contact from '@/components/Contact';
+import Resume from '@/components/Resume';
+import Profile from '@/components/Profile';
+import NavBar from '@/components/NavBar';
+import ToolBar from '@mui/material/Toolbar';
 
-import Grid from '@mui/material/Grid'
-import { Link, Element, Events, animateScroll as scroll } from 'react-scroll'
+import Grid from '@mui/material/Grid';
+import { Link, Element, Events, animateScroll as scroll } from 'react-scroll';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -23,50 +23,48 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <Grid
-          container
-          direction="row"
-          alignItems="center"
-          justifyContent="space-evenly"
-          sx={{px: 5}}
-        >
-
-          <Grid item xs={12}>
-        <Element name="Top">
-        <ToolBar />
-        <ToolBar />
-
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        justifyContent="space-evenly"
+        sx={{ px: 5, gap: 2 }}
+      >
+        <Grid item xs={12}>
+          <Element name="Top">
+            <ToolBar />
+            <ToolBar />
             <Profile />
-        </Element>
-          </Grid>
-
-          <Grid item  xs={8}>
-        <Element name="About">
-          <br />
-          <br />
-          <ToolBar />
-          <About />
-        </Element>
-            </Grid>
-          <Grid item xs={12}>
-        <Element name="Projects">
-          <ToolBar />
-          <Projects />
-        </Element>
-            </Grid>
-          <Grid item xs={8}>
-        <Element name="Resume">
-          <ToolBar />
-          <Resume />
-        </Element>
-            </Grid>
-          <Grid item xs={8}>
-        <Element name="Contact">
-          <ToolBar />
-          <Contact />
-        </Element>
-            </Grid>
+          </Element>
         </Grid>
+
+        <Grid item xs={12} sm={10} md={8}>
+          <Element name="About">
+            <br />
+            <br />
+            <ToolBar />
+            <About />
+          </Element>
+        </Grid>
+        <Grid item xs={12}>
+          <Element name="Projects">
+            <ToolBar />
+            <Projects />
+          </Element>
+        </Grid>
+        <Grid item xs={12} sm={10} md={8}>
+          <Element name="Resume">
+            <ToolBar />
+            <Resume />
+          </Element>
+        </Grid>
+        <Grid item xs={12} sm={10} md={8}>
+          <Element name="Contact">
+            <ToolBar />
+            <Contact />
+          </Element>
+        </Grid>
+      </Grid>
     </>
-  )
+  );
 }
