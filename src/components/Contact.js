5e898267569c1ada2704bc66b1aecb8a64869ccd
Alex Howard <alex-m-howard@pm.me>
2023-08-07 10:06:37 -0400

    Added grids to contact buttons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,45 +18,53 @@ const Contact = () => {
 
   return (
     <Grid container justifyContent="center" alignItems="center">
-      <Grid container item xs={12} sm={8} >
+      <Grid container item xs={12} md={4} >
         <div className="contact" style={contactStyle}>
           <Typography variant="h2" sx={{mb: 3}}>
             Contact
           </Typography>
-          <Typography variant="body1" gutterBottom>
+          
+
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid item xs={12} sm={4} >
+          <Button
+              sx={{ height: "40px", ml: '40px' }}
+              variant="contained"
+              color="primary"
+              href="mailto:alex-m-howard@pm.me"
+              target="_blank"
+              rel="noopener noreferrer">
+              
+              Email
+          </Button>
+        </Grid>
+
+        <Grid item xs={12} sm={4} >
+          <Button
+          sx={{ height: "40px", ml: '40px' }}
+          variant="contained"
+          color="primary"
+          href="https://www.linkedin.com/in/alex-m-howard/"
+          target="_blank"
+          rel="noopener noreferrer">
+          LinkedIn
+          </Button>
+        </Grid>
+          
+        <Grid item xs={12} sm={4} >
+          <Button
+          sx={{ height: "40px", ml: '40px' }}
+          variant="contained"
+          color="primary"
+          href="https://docs.google.com/uc?id=1ah7S6GIEzPNSML_u068FdVLhaJZaFSmZ&export=download"
+          target="_blank"
+          rel="noopener noreferrer">
+          Resume
+          </Button>
+        </Grid>
+      </Grid>
 
 
-<Button
-    sx={{ height: "40px", ml: '40px' }}
-    variant="contained"
-    color="primary"
-    href="mailto:alex-m-howard@pm.me"
-    target="_blank"
-    rel="noopener noreferrer">
-    
-    Email
-  </Button>
-<Button
-    sx={{ height: "40px", ml: '40px' }}
-    variant="contained"
-    color="primary"
-    href="https://www.linkedin.com/in/alex-m-howard/"
-    target="_blank"
-    rel="noopener noreferrer">
-    LinkedIn
-  </Button>
-
-    
-    <Button
-    sx={{ height: "40px", ml: '40px' }}
-    variant="contained"
-    color="primary"
-    href="https://docs.google.com/uc?id=1ah7S6GIEzPNSML_u068FdVLhaJZaFSmZ&export=download"
-    target="_blank"
-    rel="noopener noreferrer">
-    Resume
-  </Button>
-</Typography>
           <br />
           <br />
 
