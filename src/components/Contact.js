a7e0c943ace654e8cd1cc7c3100b5a868976ed0b
Alex Howard <alex-m-howard@pm.me>
2023-08-07 09:59:42 -0400

    Changed styling of contact buttons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import LinkedIn from "@mui/icons-material/LinkedIn";
 
 const Contact = () => {
   // Add a style object for the About component
@@ -21,27 +23,53 @@ const Contact = () => {
           <Typography variant="h2" sx={{mb: 3}}>
             Contact
           </Typography>
+          <Typography variant="body1" gutterBottom>
+
+
+<Button
+    sx={{ height: "40px", ml: '40px' }}
+    variant="contained"
+    color="primary"
+    href="mailto:alex-m-howard@pm.me"
+    target="_blank"
+    rel="noopener noreferrer">
+    
+    Email
+  </Button>
+<Button
+    sx={{ height: "40px", ml: '40px' }}
+    variant="contained"
+    color="primary"
+    href="https://www.linkedin.com/in/alex-m-howard/"
+    target="_blank"
+    rel="noopener noreferrer">
+    LinkedIn
+  </Button>
+
+    
+    <Button
+    sx={{ height: "40px", ml: '40px' }}
+    variant="contained"
+    color="primary"
+    href="https://docs.google.com/uc?id=1ah7S6GIEzPNSML_u068FdVLhaJZaFSmZ&export=download"
+    target="_blank"
+    rel="noopener noreferrer">
+    Resume
+  </Button>
+</Typography>
+          <br />
+          <br />
+
           <div className="calendly-inline-widget"
             data-url="https://calendly.com/alex-m-howard/30min?hide_gdpr_banner=1"
-            style={{ minWidth: '320px', minHeight: '700px', width: '100%' }}
+            style={{ minWidth: '320px', minHeight: '1000px', width: '100%' }}
           />
             <Helmet>
               <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
             </Helmet>
             <br />
             <br />
-          <Typography variant="body1" gutterBottom>
-            If you'd like to get in touch with me by email: <br/>
-              <a href="mailto:alex-m-howard@pm.me">
-                alex-m-howard@pm.me
-              </a>
-            <br />
-            <br />
-            You can also find me on{" "}
-              <a href="https://www.linkedin.com/in/alex-m-howard/" target="_blank">
-                LinkedIn
-              </a>
-          </Typography>
+          
         </div>
       </Grid>
     </Grid>
