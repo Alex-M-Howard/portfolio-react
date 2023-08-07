ffede14cabaf93e8579ccf935c98838d1517c773
Alex Howard <alex-m-howard@pm.me>
2023-08-07 11:34:02 -0400

    Changed preview picture for sharing

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" style={{width: '100vw'}}>
-      <Head />
+      <Head>
+        <meta property="og:image" content="https://media.istockphoto.com/id/1047259374/photo/programming-source-code-abstract-background.jpg?b=1&s=612x612&w=0&k=20&c=ujRPoiaJlm5U3WDWcVVa1YVlFIt6Gcjr-RstzOEPbIU=" />
+      </Head>
       <body>
         <Main />
         <NextScript />
